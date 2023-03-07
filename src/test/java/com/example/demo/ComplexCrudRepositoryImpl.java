268fefb0ed211ec69ffd15b75f812a2185883263
2023_spring <5h6sm.github@gmail.com>
2023-03-07 10:23:59 +0900

    Crud

---
+++ src/test/java/com/example/demo/ComplexCrudRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Repository;
+
+// ComplexCrudRepositoryImpl.java
+@Repository
+public class ComplexCrudRepositoryImpl implements CrudRepository {
+    @Override
+    public Object[] getAll(Object o) {
+        return new Object[0];
+    }
+
+    @Override
+    public void save(Object o) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+// ... 적당히 구현했다고 가정
+}
